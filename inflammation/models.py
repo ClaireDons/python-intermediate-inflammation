dca3335af24f9c24c0a5b2cde0f3a634ec74eb35
Claire Donnelly <claire.donnelly@knmi.nl>
2023-03-22 15:41:22 +0100

    Fixed stdev function

--- inflammation/models.py
+++ inflammation/models.py
@@ -3,7 +3,7 @@
 The Model layer is responsible for the 'business logic' part of the software.
 
 Patients' data is held in an inflammation table (2d array) where each row contains
-inflammation data for a single patient taken over a number of days 
+inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
@@ -63,5 +63,15 @@ def patient_normalise(data):
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
+
+
+def daily_stdev(data):
+    """Calculate the daily standard deviation of a 2D inflammation data array for each day.
+
+    :param data: A 2D data array with inflammation data
+    (each row has measurements for a single patient across all days)
+    :returns: An array of standard deviation values of measurements for each day.
+    """
+    return np.std(data, axis=0)
 
 
